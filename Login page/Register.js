1ab9470bf27e8fa09b1289730acd26ef172055c2
SwastikGoomber <105936811+SwastikGoomber@users.noreply.github.com>
2023-12-19 10:36:58 +0530

    improved auth

--- Login page/Register.js
+++ Login page/Register.js
@@ -5,13 +5,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const createUsername = document.querySelector(".create-username").value;
     const createPassword = document.querySelector(".create-password").value;
 
-    //signup API endpoint call
-    signupUser(createUsername, createPassword);
+    // password strength validation function
+    if (isPasswordStrong(createPassword)) {
+      // signup API endpoint here
+      signupUser(createUsername, createPassword);
+    } else {
+      // error message for weak password
+      alert(
+        "Password must be at least 8 characters long and include a number and a symbol."
+      );
+    }
   });
 });
 
-function redirtoLogin() {
-  window.location.href = "login.html";
+function isPasswordStrong(password) {
+  // min 8 chars, at least one number, at least one symbol
+  const passwordRegex =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  return passwordRegex.test(password);
 }
 
 async function signupUser(username, password) {
@@ -36,10 +47,16 @@ async function signupUser(username, password) {
     const data = await response.json();
     console.log(data);
 
-    // If signup successful
-    //do shit here
+    // Display success message
+    const successMessage = document.getElementById("reg-success-message");
+    successMessage.style.display = "block";
+
+    // Redirect to the login page after a delay (e.g., 3 seconds)
+    setTimeout(() => {
+      window.location.href = "login.html";
+    }, 3000); // Adjust the delay as needed
   } catch (error) {
     console.error("Error:", error.message);
-    //error goes here
+    // Handle error, e.g., show an error message to the user
   }
 }
